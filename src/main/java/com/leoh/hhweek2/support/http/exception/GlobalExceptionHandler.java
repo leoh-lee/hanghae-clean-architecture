81bafb93a644e647c36662b46c95091c444cad7c
nimoh <spakers38@gmail.com>
2024-12-26 23:00:13 +0900

    feat: GlobalExceptionHandler에 LectureNotFoundException과 Exception에 대한 핸들링 메서드 추가

--- src/main/java/com/leoh/hhweek2/support/http/exception/GlobalExceptionHandler.java
+++ src/main/java/com/leoh/hhweek2/support/http/exception/GlobalExceptionHandler.java
@@ -1,8 +1,22 @@
 package com.leoh.hhweek2.support.http.exception;
 
+import com.leoh.hhweek2.domain.exception.LectureNotFoundException;
+import com.leoh.hhweek2.support.http.response.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(LectureNotFoundException.class)
+    public ApiResponse<Void> lectureNotFoundException() {
+        return new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), "해당 특강을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<Void> exception() {
+        return new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "알 수 없는 예외가 발생하였습니다.");
+    }
 
 }
