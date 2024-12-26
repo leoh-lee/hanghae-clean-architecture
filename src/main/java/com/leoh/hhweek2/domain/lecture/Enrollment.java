48b9865756e66406f53150de56426e95ad817dfb
nimoh <spakers38@gmail.com>
2024-12-27 05:16:00 +0900

    feat: Enrollment, Lecture 테이블에 대한 인덱스 추

--- src/main/java/com/leoh/hhweek2/domain/lecture/Enrollment.java
+++ src/main/java/com/leoh/hhweek2/domain/lecture/Enrollment.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
         @UniqueConstraint(
                 name="lecture_user_uk",
                 columnNames={"lecture_id","user_id"}
-        )})
+        )}, indexes = @Index(name = "idx_user_id_enrollment", columnList = "user_id"))
 public class Enrollment extends BaseEntity {
 
     @Id
